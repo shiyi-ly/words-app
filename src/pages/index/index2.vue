4a97db6ed1377fbaca1dc40414d3159104982722
失忆 <1282760680@qq.com>
2023-02-08 15:15:03 +0800

    出书后

---
+++ src/pages/index/index2.vue
@@ -0,0 +1,32 @@
+<template>
+	<view class="content">
+    <view class="rpx">rpx</view>
+    <view class="vw">vw</view>
+    <view class="sass">sass</view>
+  </view>
+
+</template>
+
+<script>
+	
+</script>
+
+<style lang="scss">
+  /* rpx 小程序中的单位 750rpx = 屏幕的宽度 */
+	.rpx{
+    width: 750rpx;
+    height: 100px;
+    background-color: aqua;
+  }
+  .vw {
+    /*vw h5单位 100vw = 屏幕的宽度 100vh = 屏幕的高度 */
+    width: 50vw;
+    height: 100px;
+    background-color: tan;
+  }
+  .content {
+    .sass {
+      background-color: red;
+    }
+  }
+  </style>

b33285bd9990c277fbad765b108ca02c32b8c33f
失忆 <1282760680@qq.com>
2023-02-09 09:22:29 +0800

    提交

---
+++ src/pages/index/index3.vue
@@ -0,0 +1,20 @@
+<template>
+	<view class="content">
+    <view>{{msg}}
+  </view>
+
+</template>
+
+<script>
+	export default {
+    data() {
+      return {
+        msg:"失忆"
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
